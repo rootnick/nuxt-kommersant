61721ce18533c252a401661287312ea473c42071
rootnick <rootnick1@gmail.com>
2022-09-19 17:26:08 +0300

    feat: component DocPageData fish

---
+++ components/DocPageData.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>DocPageData---2</div>
+</template>
+<script lang="ts">
+  import Vue from 'vue'
+  export default Vue.extend({
+    name: "DocPageData"
+  })
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,28 @@
 <template>
-  <div>docPageDataShort</div>
+  <div>
+    <!-- <div>docPageDataShort</div>
+      <div v-for="(element,index) in docPageDataShort" :key="index">
+        <div>{{element}}</div>
+      </div>
+    <div>docPageData</div>
+      <div v-for="(element,index) in docPageData" :key="index">
+        <div>{{element}}</div>
+    </div> -->
+    <DocPageData/>
+  </div>
 </template>
 
 <script lang="ts">
 import { data } from 'browserslist'
 import Vue from 'vue'
 import {docPageDataShort, docPageData} from './documentPageData/data'
+import DocPageData from "~/components/DocPageData";
 
 export default Vue.extend({
   name: 'IndexPage',
+  components:{
+    DocPageData
+  },
   computed:{
     docPageDataShort(){
       return docPageDataShort.data.content.docBodyElements
