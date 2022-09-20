ad41239bdab8c2a75a2c9317c8bdd585c3df2379
rootnick <rootnick1@gmail.com>
2022-09-20 18:11:22 +0300

    feat: add document-full page

---
+++ pages/document-full.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        docPageMorozov
+    </div>
+</template>
+  
+<script lang="ts">
+import { data } from 'browserslist'
+import Vue from 'vue'
+
+export default Vue.extend({
+    name: "documentFull",
+    async asyncData({ $http }) {
+        const test = await $http.$get('/api/docPageData')
+        return { users: test }
+    }
+
+})
+</script>
+  
