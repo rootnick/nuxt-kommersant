0b9333a05c3144abfdc7296ce895926158da0197
rootnick <rootnick1@gmail.com>
2022-09-20 14:40:38 +0300

    fix: rename function

--- api/index.js
+++ api/index.js
@@ -3,14 +3,14 @@ import { docPageDataShort, docPageData } from './documentPageData/data.ts'
 
 const app = express()
 
-const content = (dataRoot) => dataRoot.data.content.docBodyElements;
+const getContent = (dataRoot) => dataRoot.data.content.docBodyElements;
 
 app.get('/docPageDataShort', (req, res) => {
-  res.send(content(docPageDataShort))
+  res.send(getContent(docPageDataShort))
 })
 
 app.get('/docPageData', (req, res) => {
-  res.send(content(docPageData))
+  res.send(getContent(docPageData))
 })
 
 module.exports = app
