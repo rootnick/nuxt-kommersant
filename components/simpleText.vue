6b21c97468834e7f6dfc9a0e5adf5a076a9625aa
rootnick <rootnick1@gmail.com>
2022-09-27 18:52:08 +0300

    feat: add simpletext

---
+++ components/simpleText.vue
@@ -0,0 +1,18 @@
+<template>
+  <p>{{ text }}</p>
+</template>
+
+<script lang='ts'>
+import Vue from 'vue'
+import type { ArticleLong } from '~/static/types'
+
+export default Vue.extend({
+  name: 'simpleText',
+  props: {
+    text: {
+      type: String as () => ArticleLong.PlainTextElement,
+      required: true,
+    },
+  },
+})
+</script>
--- components/widgetType/paraWidgetType.vue
+++ components/widgetType/paraWidgetType.vue
@@ -1,16 +1,19 @@
 <template>
   <p class="doc__text">
-    <span v-for="(element, index) in bodyElements" :key="index">
-      {{ element }}
-    </span>
+    <template v-for="(element) in bodyElements">
+     {{element}}
+    </template>
   </p>
 </template>
 <script lang='ts'>
 import Vue from 'vue'
 import type { ArticleLong } from '~/static/types'
-
+import simpleText from '~/components/simpleText.vue'
 export default Vue.extend({
   name: 'paraWidgetType',
+  components: {
+    simpleText,
+  },
   props: {
     bodyElements: {
       type: Array as () => ArticleLong.DocBodyElement[],
